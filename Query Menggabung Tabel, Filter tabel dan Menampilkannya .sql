9eb63d5feab957b1c0771759ed6090a04ab6740a
Alvitariani Khairinia Kusuma <48576002+alvitarianikk@users.noreply.github.com>
2020-05-22 13:24:37 +0700

    Update Query Menggabung Tabel, Filter tabel dan Menampilkannya .sql

--- Query Menggabung Tabel, Filter tabel dan Menampilkannya .sql
+++ Query Menggabung Tabel, Filter tabel dan Menampilkannya .sql
@@ -1,3 +1,3 @@
-SELECT d.*, s.*, rc.description 
-FROM autodebet_new.payment_due d, autodebet_new.payment_success s, autodebet_new.rc rc 
-	where d.id = s.payment_due_id and rc.id = s.rc_id and d.executed_at in ("2020-04-05","2020-04-08") and d.id in ('272143','272160','272274','272282','272296','272309','278045','278046','278069','278070','278073','278074');
+SELECT d.*, s.*, buku.description 
+FROM perpus.judul d, perpus.genre s, perpus.buku buku 
+	where d.id = s.perpus_id and buku.id = s.buku_id and d.executed_at in ("2020-04-05","2020-04-08") and d.id in ('272143','272160','272274','272282','272296','272309','278045','278046','278069','278070','278073','278074');
