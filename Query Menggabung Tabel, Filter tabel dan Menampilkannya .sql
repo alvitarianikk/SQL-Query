3f145a1e0ca0f63e48d1e79bc7a82982a17fc29a
Alvitariani Khairinia Kusuma <48576002+alvitarianikk@users.noreply.github.com>
2020-05-22 13:25:45 +0700

    Update Query Menggabung Tabel, Filter tabel dan Menampilkannya .sql

--- Query Menggabung Tabel, Filter tabel dan Menampilkannya .sql
+++ Query Menggabung Tabel, Filter tabel dan Menampilkannya .sql
@@ -1,3 +1,3 @@
 SELECT d.*, s.*, buku.description 
 FROM perpus.judul d, perpus.genre s, perpus.buku buku 
-	where d.id = s.perpus_id and buku.id = s.buku_id and d.executed_at in ("2020-04-05","2020-04-08") and d.id in ('272143','272160','272274','272282','272296','272309','278045','278046','278069','278070','278073','278074');
+	where d.id = s.perpus_id and buku.id = s.buku_id and d.upload_at in ("2020-04-05","2020-04-08") and d.id in ('272143','272160','272274','272282','272296','272309','278045','278046','278069','278070','278073','278074');
