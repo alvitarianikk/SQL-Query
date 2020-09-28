a4d7c55a37cf596d2a794c3906863eb41bf788cb
Alvitariani Khairinia Kusuma <48576002+alvitarianikk@users.noreply.github.com>
2020-09-28 11:16:25 +0700

    Update Query Inner Join.sql

--- Query Inner Join.sql
+++ Query Inner Join.sql
@@ -7,5 +7,5 @@ id_buku,
 judul,
 kategori 
 	FROM 
-	buku_alvitarianikk.buku b inner join
-	buku_alvitarianikk.kategori kat ON b.id_kategori=kat.id_kategori;
+	buku.buku b inner join
+	buku.kategori kat ON b.id_kategori=kat.id_kategori;
